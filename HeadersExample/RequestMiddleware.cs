d6d737562093e4d0a3d4cce9783618a6315342fa
Damian McGoohan <damian.mcgoohan@sita.aero>
2022-04-09 10:27:45 +0100

    first commit

---
+++ HeadersExample/RequestMiddleware.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Extensions.Primitives;
+
+namespace HeadersExample;
+
+public class RequestMiddleware
+{
+    private readonly RequestDelegate _next;
+
+    public RequestMiddleware(RequestDelegate next)
+    {
+        _next = next;
+    }
+
+    public async Task InvokeAsync(HttpContext context)
+    {
+        if (context.Request.Headers.Any(x => x.Key == "Device-Id"))
+        {
+            Console.WriteLine($"Device id: {context.Request.Headers["Device-Id"]}");
+        }
+        else
+        {
+            Console.WriteLine("Adding device id to header");
+            context.Request.Headers.Add("Device-Id", StringValues.Empty);
+        }
+
+        await _next(context);
+    }
+}
+
+public static class RequestMiddlewareExtensions
+{
+    public static IApplicationBuilder UseCustomRequestHeaders(this IApplicationBuilder builder)
+    {
+        return builder.UseMiddleware<RequestMiddleware>();
+    }
+}
